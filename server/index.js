fde1634e55a6a8e2ef40ee7e217961222c0192b5
Anirudhwa Ankon <ankon495@gmail.com>
2023-05-31 17:59:59 +0600

    fixed .gitignore not working properly

--- server/index.js
+++ server/index.js
@@ -1,9 +1,1 @@
-var express = require('express');
-var app = express();
-var port = 3000;
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-app.listen(port, function () {
-    console.log("Example app listening on port ".concat(port));
-});
+//write express connection code here
