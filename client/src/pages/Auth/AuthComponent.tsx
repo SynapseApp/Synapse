332ff59325dda9e69f7edb6bb03a689042f3b141
Anirudhwa Ankon <ankon495@gmail.com>
2023-06-09 01:40:17 +0600

    Fixed up front end and added method to get input data

--- client/src/pages/Auth/AuthComponent.tsx
+++ client/src/pages/Auth/AuthComponent.tsx
@@ -1,16 +1,19 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight, faG } from "@fortawesome/free-solid-svg-icons";
-import InputGroup from "../../assets/InputGroup";
-import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faG } from '@fortawesome/free-solid-svg-icons';
+import InputGroup from '../../assets/InputGroup';
+import { useState } from 'react';
+import { FormEvent } from 'react';
 
 export default function AuthComponent() {
-  const [method, setMethod] = useState("Login");
+  const [method, setMethod] = useState('Login');
+  const [valueEmail, setValueEmail] = useState('');
+  const [valuePassword, setValuePassword] = useState('');
 
-  function handleSubmit(event: Event) {
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    if (method === "login") {
-      // Login the user
+    if (method === 'Login') {
+      //login user
     } else {
       // Register the user
     }
@@ -18,39 +21,30 @@ export default function AuthComponent() {
 
   return (
     <>
-      <form id="Login-Component" className="">
-        <h6>
-          {method === "Login"
-            ? "Login To Your Account"
-            : "Register A New Account"}
-        </h6>
+      <form id="Login-Component" className="" onSubmit={handleSubmit}>
+        <h6>{method === 'Login' ? 'Login To Your Account' : 'Register A New Account'}</h6>
 
-        <button
-          id="Google-Auth-Button"
-          className="gradient-box glow-effect-border"
-        >
+        <button id="Google-Auth-Button" className="gradient-box glow-effect-border">
           <FontAwesomeIcon icon={faG} size="xl" />
-          Sign Up With Goggle
+          {method} With Google
         </button>
 
-        <InputGroup type="email" placeholder="Email" />
-        <InputGroup type="password" placeholder="Password" />
-        {method === "Login" && (
-          <p className="gradient-border">Forgot Password?</p>
-        )}
+        <InputGroup type="email" placeholder="Email" setValueInput={setValueEmail} />
+        <InputGroup type="password" placeholder="Password" setValueInput={setValuePassword} />
+        {method === 'Login' && <p className="gradient-border">Forgot Password?</p>}
         <button id="Submit-Button" className="gradient-btn glow-effect">
           Continue
           <FontAwesomeIcon icon={faArrowRight} size="xl" />
         </button>
 
         <p
-          className="gradient-border"
           onClick={() => {
-            if (method === "Login") setMethod("Register");
-            else setMethod("Login");
+            if (method === 'Login') setMethod('Sign up');
+            else setMethod('Login');
           }}
         >
-          Dont have a account? <b>Sign Up</b>
+          {method === 'Login' ? 'Already have an account? ' : 'Create an account '}
+          <b className="gradient-border">{method === 'Login' ? 'Sign up' : 'Login'}</b>
         </p>
       </form>
     </>
