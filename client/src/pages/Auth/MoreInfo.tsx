ba29ccaf2e161891e9c4a786d517e2ac4cecc0cf
Aditya81016 <aditya.jaiswal.81016@gmail.com>
2023-06-17 07:27:06 +0530

    setuped form to take profile info from user

---
+++ client/src/pages/Auth/MoreInfo.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import InputGroup from "../../assets/InputGroup";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+export default function MoreInfo() {
+  const [username, setUsername] = useState("");
+
+  function takeImgInput() {
+    const input = document.getElementById("img-input") as HTMLInputElement;
+    input.click();
+  }
+
+  return (
+    <>
+      <form id="MoreInfo">
+        <h6>Your Profile Details</h6>
+        <div className="pfp" onClick={takeImgInput}></div>
+        <input type="file" id="img-input" name="img" accept="image/*" />
+        <InputGroup
+          type="text"
+          placeholder="Username"
+          setValueInput={setUsername}
+        />
+        <button id="Submit-Button" className="gradient-btn glow-effect">
+          Continue
+          <FontAwesomeIcon icon={faArrowRight} size="xl" />
+        </button>
+      </form>
+    </>
+  );
+}
