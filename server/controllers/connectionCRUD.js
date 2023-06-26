c7a1f21889d3d816a18de0250542b8ad3b8cb94a
Aditya81016 <aditya.jaiswal.81016@gmail.com>
2023-06-26 22:11:56 +0530

    connection routes added

---
+++ server/controllers/connectionCRUD.js
@@ -0,0 +1,21 @@
+// Collection CRUD here
+
+exports.createCollection = async function createCollection() {
+  return "createCollection()"
+}
+
+exports.findConnection = async function findConnection() {
+  return "findConnection()"
+}
+
+exports.findUserConnections = async function findUserConnections() {
+  return "findUserConnections()"
+}
+
+exports.updateConnection = async function updateConnection() {
+  return "updateConnection()"
+}
+
+exports.deleteConnection = async function deleteConnection() {
+  return "deleteConnection()"
+}
---
+++ server/models/connectionModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+// Create a new Mongoose schema
+const Schema = mongoose.Schema;
+
+// Define the connections schema
+const connectionSchema = new Schema({
+  userOne: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  userTwo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  relation: {
+    type: String,
+    required: true
+  }
+});
+
+// Create and export the Connection model based on the connection schema
+module.exports = mongoose.model('Connection', connectionSchema);
