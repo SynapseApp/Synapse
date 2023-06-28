a197ef62bc98ef0ef7efd05ad2123c4e9f1eeaef
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-06-29 01:15:09 +0300

    removing the unnaccery async function

--- server/seeders/1-users/users.js
+++ server/seeders/1-users/users.js
@@ -1,24 +1,18 @@
 const path = require("path");
 const fs = require("fs");
 
-async function create_users() {
-	let users = [];
-	const emails = await JSON.parse(
-		fs.readFileSync(path.resolve(__dirname, "../", "emails.json")),
-		{ encoding: "utf8" }
-	);
-	for (let i = 0; i < emails.length; i++) {
-		const email = emails[i].email;
-		users.push({
-			email,
-			user_name: email.split("@")[0],
-			name: email.split("@")[0].replaceAll("_", " "),
-		});
-	}
-	return users;
+let users = [];
+const emails = JSON.parse(
+	fs.readFileSync(path.resolve(__dirname, "../", "emails.json")),
+	{ encoding: "utf8" }
+);
+for (let i = 0; i < emails.length; i++) {
+	const email = emails[i].email;
+	users.push({
+		email,
+		user_name: email.split("@")[0],
+		name: email.split("@")[0].replaceAll("_", " "),
+	});
 }
 
-create_users().then((users) => {
-	console.log(users);
-	module.exports = users;
-});
+module.exports = users;
