9bc209f98e38800b731951781f6350e35651fd0a
AhFa <contactocoachahfa@gmail.com>
2023-06-28 10:54:00 +0200

    User Info Fixed

--- client/src/pages/Home/MessageComponents/ChatMenu.jsx
+++ client/src/pages/Home/MessageComponents/ChatMenu.jsx
@@ -25,11 +25,13 @@ const ChatMenu = () => {
     const element = document.querySelector('.chat-info');
     if (element) {
       element.classList.add('hidden');
-      document.querySelector(".chat-menu").style.width = "100%"      
+      document.querySelector(".chat-menu").style.width = "100%"
+      
     }
   }
 
   return (
+    <>
     <div className="chat-menu">
       <div className="chat-nav">
         <div className="chat-left">
@@ -45,17 +47,19 @@ const ChatMenu = () => {
           <FontAwesomeIcon className="user-icon info-icon" size="2xl" icon={faCircleInfo} onClick={removeHiddenClass} />
         </div>
       </div>
-      <div className="chat-info hidden">
+      <div className="component-div">
+        <ChatContainerComponent />
+      </div>
+    </div>
+    <div className="chat-info hidden">
         <FontAwesomeIcon className="x-icon" icon={faX} onClick={addHiddenClass} />
         <img className="chat-info-pic" src={userChats.picture} />
         <h2 className="chat-info-header">{userChats.friend}</h2>
         <ChatInfoButtons />
       </div>
 
-      <div className="component-div">
-        <ChatContainerComponent />
-      </div>
-    </div>
+    </>
+    
   );
 };
 
