ccbfc210253109ccaa4297e6decb528b8a5f3567
Ank <ankon495@gmail.com>
2023-07-04 04:35:16 +0600

    Added SearchTerm connection search and search only on submit

--- client/src/pages/Home/MessageComponents/ChatContainerComponent.jsx
+++ client/src/pages/Home/MessageComponents/ChatContainerComponent.jsx
@@ -1,41 +1,36 @@
-import {
-  faFaceLaugh,
-  faImage,
-  faRightLong,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFaceLaugh, faImage, faRightLong } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ChatContainerComponent = () => {
   let conversation = [
     {
-      msgSender: "You",
-      msgText: "Adi bumbum",
+      msgSender: 'You',
+      msgText: 'Adi bumbum',
       date: new Date(),
     },
     {
-      msgSender: "Adi",
-      msgText: "my bumbum",
+      msgSender: 'Adi',
+      msgText: 'my bumbum',
     },
     {
-      msgSender: "Adi",
-      msgText: "I had biryani benchod",
+      msgSender: 'Adi',
+      msgText: 'sad',
     },
     {
-      msgSender: "You",
-      msgText: "noice",
+      msgSender: 'You',
+      msgText: 'noice',
     },
     {
-      msgSender: "Adi",
-      msgText: "hello",
+      msgSender: 'Adi',
+      msgText: 'hello',
     },
   ];
 
-  const printMessages = function(conversation) {
+  const printMessages = function (conversation) {
     const renderedMessages = [];
 
     for (let i = 0; i < conversation.length; i++) {
-      const messageClassName =
-        conversation[i].msgSender === "You" ? "my-msg" : "receiving-msg";
+      const messageClassName = conversation[i].msgSender === 'You' ? 'my-msg' : 'receiving-msg';
       const renderedMessage = (
         <div className={messageClassName} key={i}>
           <div className="msg-div">
--- client/src/pages/Home/SearchComponents/searchedProfile.jsx
+++ client/src/pages/Home/SearchComponents/searchedProfile.jsx
@@ -1,44 +1,42 @@
-import UserProfile from "./userProfile";
-import { useState, useEffect } from "react";
-import { useContext } from "react";
-import UserContext from "../../../Contexts/userContext";
+import UserProfile from './userProfile';
+import { useState, useEffect } from 'react';
+import { useContext } from 'react';
+import UserContext from '../../../Contexts/userContext';
+import PropTypes from 'prop-types';
 
 const SearchedProfile = (props) => {
   const [showConnectionProfile, setShowConnectionProfile] = useState(false);
   const [showStrangerProfile, setShowStrangerProfile] = useState(false);
   const [selectedChatIndex, setSelectedChatIndex] = useState(null);
-  const [connectionStatus, setConnectionStatus] = useState(""); // New state variable
+  const [connectionStatus, setConnectionStatus] = useState(''); // New state variable
   const [dummyState, setDummyState] = useState(0); // Dummy state variable
 
   const user = useContext(UserContext);
 
-  const handleShowProfile = function(index, section) {
+  const handleShowProfile = function (index, section) {
     setSelectedChatIndex(index);
 
-    if (section === "connection") {
+    if (section === 'connection') {
       setShowConnectionProfile(!showConnectionProfile);
       setShowStrangerProfile(false);
-    } else if (section === "stranger") {
+    } else if (section === 'stranger') {
       setShowStrangerProfile(!showStrangerProfile);
       setShowConnectionProfile(false);
     }
   };
 
   const [strangersSearchedResult, setStrangersSearchedResult] = useState([]);
-  const [connectionsSearchedResult, setConnectionsSearchedResult] = useState(
-    []
-  );
+  const [connectionsSearchedResult, setConnectionsSearchedResult] = useState([]);
 
   useEffect(() => {
-    console.log("")
     fetchData();
-  }, [props.searchTerm]); // Reload the component when connection status changes
+  }, []); // Reload the component when connection status changes
 
   const fetchData = async () => {
-    const response = await fetch("http://localhost:3000/connection/search", {
-      method: "POST",
+    const response = await fetch('http://localhost:3000/connection/search', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         id: user._id,
@@ -47,53 +45,28 @@ const SearchedProfile = (props) => {
     });
     const data = await response.json();
 
-    const connectionDataPromises = data.connections.map(async (connection) => {
-      const otherUserId =
-        connection.userOne === user._id
-          ? connection.userTwo
-          : connection.userOne;
-      const userResponse = await fetch(
-        `http://localhost:3000/user/${otherUserId}`
-      );
-      const userData = await userResponse.json();
-      return {
-        userData,
-      };
-    });
-
-    const connectionUserData = await Promise.all(connectionDataPromises);
-
-    const connectionsWithUserData = connectionUserData.map(({ userData }) => ({
-      userData,
-    }));
-
-    setConnectionsSearchedResult(connectionsWithUserData);
+    setConnectionsSearchedResult(data.connections);
     setStrangersSearchedResult(data.strangers);
   };
 
   function truncateText(text, maxLength) {
     if (text.length > maxLength) {
-      return text.substring(0, maxLength) + "...";
+      return text.substring(0, maxLength) + '...';
     }
     return text;
   }
 
-  const printStrangerChats = function() {
+  const printStrangerChats = function () {
     const renderedChats = [];
 
     for (let i = 0; i < strangersSearchedResult.length; i++) {
-      const truncatedDisplayName =
-        truncateText(strangersSearchedResult[i].displayName, 18) || "";
-      const pictureUrl = "https://example.com/random-image.png"; // Use a random image URL
+      const truncatedDisplayName = truncateText(strangersSearchedResult[i].displayName, 18) || '';
+      const pictureUrl = 'https://example.com/random-image.png'; // Use a random image URL
 
       renderedChats.push(
         <div key={i}>
           <div className="render-chat">
-            <div
-              className="chat"
-              id={`${i}_chatStranger`}
-              onClick={() => handleShowProfile(i, "stranger")}
-            >
+            <div className="chat" id={`${i}_chatStranger`} onClick={() => handleShowProfile(i, 'stranger')}>
               <img src={pictureUrl} alt="Profile" />
               <div className="chat-text" onClick={removeHiddenChatMenu}>
                 <p className="contact-name">{truncatedDisplayName}</p>
@@ -121,44 +94,28 @@ const SearchedProfile = (props) => {
     return renderedChats;
   };
 
-  const printConnectionChats = function() {
+  const printConnectionChats = function () {
     const renderedChats = [];
+
     for (let i = 0; i < connectionsSearchedResult.length; i++) {
-      const truncatedDisplayName =
-        truncateText(
-          connectionsSearchedResult[i].userData.data.user.displayName,
-          18
-        ) || "";
-      const pictureUrl = "https://example.com/random-image.png"; // Use a random image URL
+      const connection = connectionsSearchedResult[i].userData;
+      if (!connection) continue; // Skip if connection is null or undefined
+
+      const truncatedDisplayName = truncateText(connection.displayName, 18) || '';
+      const pictureUrl = 'https://example.com/random-image.png';
 
       renderedChats.push(
         <div key={i}>
           <div className="render-chat">
-            <div
-              className="chat"
-              id={`${i}_chatConnection`}
-              onClick={() => handleShowProfile(i, "connection")}
-            >
+            <div className="chat" id={`${i}_chatConnection`} onClick={() => handleShowProfile(i, 'connection')}>
               <img src={pictureUrl} alt="Profile" />
               <div className="chat-text" onClick={removeHiddenChatMenu}>
                 <p className="contact-name">{truncatedDisplayName}</p>
-                <p>
-                  {connectionsSearchedResult[i]?.userData.data.user.username}
-                </p>
+                <p>{connection.username}</p>
               </div>
             </div>
             {showConnectionProfile && selectedChatIndex === i && (
-              <UserProfile
-                id={`${i}_userConnection`} // Rename the prop to a different name, e.g., 'id' or 'profileKey'
-                userId2={connectionsSearchedResult[i].userData.data.user._id}
-                displayName={truncatedDisplayName}
-                user={connectionsSearchedResult[i].userData.data.user.username}
-                picture={pictureUrl}
-                status="connected" // Set status as "friend"
-                description="Hardcoded description" // Use a hardcoded value for description
-                setConnectionStatus={setConnectionStatus}
-                setDummyState={setDummyState}
-              />
+              <UserProfile id={`${i}_userConnection`} userId2={connection._id} displayName={truncatedDisplayName} user={connection.username} picture={pictureUrl} status="connected" description="Hardcoded description" setConnectionStatus={setConnectionStatus} setDummyState={setDummyState} />
             )}
           </div>
         </div>
@@ -169,9 +126,9 @@ const SearchedProfile = (props) => {
   };
 
   function removeHiddenChatMenu() {
-    const element = document.querySelector < HTMLElement > ".chat-menu";
+    const element = document.querySelector < HTMLElement > '.chat-menu';
     if (element) {
-      element.classList.remove("hidden");
+      element.classList.remove('hidden');
     }
   }
 
@@ -192,6 +149,10 @@ const SearchedProfile = (props) => {
   }, [connectionStatus, dummyState]);
 
   return <div>{renderedChats}</div>;
+};
+
+SearchedProfile.propTypes = {
+  searchTerm: PropTypes.string.isRequired,
 };
 
 export default SearchedProfile;
