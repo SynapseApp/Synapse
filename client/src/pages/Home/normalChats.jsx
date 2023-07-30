0a51516e3d434a454e6a7ac78ad56f1d768a4b68
Ank <ankon495@gmail.com>
2023-07-31 00:33:52 +0600

    Made connections into normal chats

--- client/src/pages/Home/normalChats.jsx
+++ client/src/pages/Home/normalChats.jsx
@@ -1,42 +1,53 @@
 // import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { useContext, useState } from "react";
-// import UserContext from "../../../Contexts/userContext";
+import { useContext, useState } from 'react';
+import UserContext from '../../Contexts/userContext';
+
+import { useEffect } from 'react';
 
 const NormalChats = () => {
-  const userChats = [
-    {
-      friend: "Aditya",
-      message: "hello",
-      picture:
-        "https://www.dreshare.com/wp-content/uploads/2021/02/Dhar-Mann-and-his-father-Baljit-Singh-Mann.jpg",
-    },
-    {
-      friend: "Jalail Login Page",
-      message: "real based player",
-      picture:
-        "https://library.sportingnews.com/styles/crop_style_16_9_mobile_2x/s3/2022-12/Lionel%20Messi%20-%20World%20Cup%20Final%202022%20penalty%20celebration%20vs%20France%20-%20181222-16x9.jpg?itok=_U5r3wZ4",
-    },
-    {
-      friend: "Ank",
-      message: "ankirduwa",
-      picture:
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
-    },
-    {
-      friend: "Ahmedabad",
-      message: "fortnite",
-      picture:
-        "https://www.elconfidencialdigital.com/asset/zoomcrop,1280,720,center,center/media/elconfidencialdigital/images/2017/06/08/ECDIMA20170608_0005_1.jpg",
-    },
-  ];
+  const [connectionsArr, setConnectionsArr] = useState([]);
+  const user = useContext(UserContext);
 
-  //   const user = useContext(UserContext);
-  //   console.log(user);
+  useEffect(() => {
+    searchConnections();
+  }, []);
+
+  const searchConnections = async function () {
+    const response = await fetch('http://localhost:3000/connection/searchConnections', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: user._id,
+      }),
+    });
+    const data = await response.json();
+    const connectionsId = [];
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].userOne === user._id) {
+        connectionsId.push(data[i].userTwo);
+      } else if (data[i].userTwo === user._id) {
+        connectionsId.push(data[i].userOne);
+      }
+    }
+
+    const tmpArr = [];
+
+    for (let i = 0; i < connectionsId.length; i++) {
+      const response = await fetch(`http://localhost:3000/user/${connectionsId[i]}`);
+      const userData = await response.json();
+      tmpArr.push(userData);
+    }
+    setConnectionsArr(tmpArr);
+  };
+
+  console.log(connectionsArr);
 
   function truncateText(text, maxLength) {
     if (text.length > maxLength) {
-      return text.substring(0, maxLength) + "...";
+      return text.substring(0, maxLength) + '...';
     }
     return text;
   }
@@ -44,16 +55,21 @@ const NormalChats = () => {
   const printChats = function () {
     const renderedChats = [];
 
-    for (let i = 0; i < userChats.length; i++) {
-      userChats[i].friend = truncateText(userChats[i].friend, 18);
-      userChats[i].message = truncateText(userChats[i].message, 20);
+    if (!connectionsArr) {
+      return (
+        <div>
+          <p>No connections found</p>
+        </div>
+      );
+    }
 
+    for (let i = 0; i < connectionsArr.length; i++) {
       renderedChats.push(
         <div className="chat" key={i}>
-          <img src={userChats[i].picture} alt="Profile" />
+          <img src="https://media.discordapp.net/attachments/1111323966691352629/1133682113699381288/20230726_141636.jpg?width=295&height=623" alt="Profile" />
           <div className="chat-text" onClick={removeHiddenChatMenu}>
-            <p className="contact-name">{userChats[i].friend}</p>
-            <p>{userChats[i].message}</p>
+            <p className="contact-name">{truncateText(connectionsArr[i].data.user.displayName, 18)}</p>
+            <p>default text</p>
           </div>
         </div>
       );
@@ -63,9 +79,9 @@ const NormalChats = () => {
   };
 
   function removeHiddenChatMenu() {
-    const element = document.querySelector < HTMLElement > ".chat-menu";
+    const element = document.querySelector < HTMLElement > '.chat-menu';
     if (element) {
-      element.classList.remove("hidden");
+      element.classList.remove('hidden');
     }
   }
 
