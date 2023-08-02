ed43f0222505e3fde99d067d5b7038b93505632e
Ank <ankon495@gmail.com>
2023-08-02 17:41:30 +0600

    Cleaned up redundant props

--- client/src/pages/Home/MessageComponents/ChatMenu.jsx
+++ client/src/pages/Home/MessageComponents/ChatMenu.jsx
@@ -4,55 +4,56 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import React from "react";
 import ChatContainerComponent from './ChatContainerComponent';
 import ChatInfoButtons from './ChatInfoButtons';
+import PropTypes from 'prop-types';
 
-const ChatMenu = ({userObject}) => {
-
+const ChatMenu = ({ userObject }) => {
   function removeHiddenClass() {
     const element = document.querySelector('.chat-info');
     if (element) {
       element.classList.remove('hidden');
-      document.querySelector(".chat-menu").style.width = "82%"
+      document.querySelector('.chat-menu').style.width = '82%';
     }
   }
   function addHiddenClass() {
     const element = document.querySelector('.chat-info');
     if (element) {
       element.classList.add('hidden');
-      document.querySelector(".chat-menu").style.width = "100%"
-
+      document.querySelector('.chat-menu').style.width = '100%';
     }
   }
   return (
     <>
-    <div className="chat-menu">
-      <div className="chat-nav">
-        <div className="chat-left">
-          <img className="user-picture" src='https://media.discordapp.net/attachments/1111323966691352629/1133682113699381288/20230726_141636.jpg?width=295&height=623'></img>
-          <div className="user-info">
-            <h6 className="user-username">{userObject.user === undefined ? "undefined" : userObject.user.displayName}</h6> 
-            <p className="user-status">{userObject.user === undefined ? "undefined" : userObject.user.id}</p> 
+      <div className="chat-menu">
+        <div className="chat-nav">
+          <div className="chat-left">
+            <img className="user-picture" src="https://media.discordapp.net/attachments/1111323966691352629/1133682113699381288/20230726_141636.jpg?width=295&height=623"></img>
+            <div className="user-info">
+              <h6 className="user-username">{userObject.user === undefined ? 'undefined' : userObject.user.displayName}</h6>
+              <p className="user-status">{userObject.user === undefined ? 'undefined' : userObject.user.id}</p>
+            </div>
+          </div>
+          <div className="chat-right">
+            <FontAwesomeIcon className="user-icon" size="lg" icon={faPhone} />
+            <FontAwesomeIcon className="user-icon" size="lg" icon={faVideo} />
+            <FontAwesomeIcon className="user-icon info-icon" size="lg" icon={faCircleInfo} onClick={removeHiddenClass} />
           </div>
         </div>
-        <div className="chat-right">
-          <FontAwesomeIcon className="user-icon" size="lg" icon={faPhone} />
-          <FontAwesomeIcon className="user-icon" size="lg" icon={faVideo} />
-          <FontAwesomeIcon className="user-icon info-icon" size="lg" icon={faCircleInfo} onClick={removeHiddenClass} />
+        <div className="component-div">
+          <ChatContainerComponent />
         </div>
       </div>
-      <div className="component-div">
-        <ChatContainerComponent />
-      </div>
-    </div>
-    <div className="chat-info hidden">
+      <div className="chat-info hidden">
         <FontAwesomeIcon className="x-icon" icon={faX} onClick={addHiddenClass} />
         <img className="chat-info-pic" src="https://media.discordapp.net/attachments/1111323966691352629/1133682113699381288/20230726_141636.jpg?width=295&height=623" />
-        <h2 className="chat-info-header">{userObject.user === undefined ? "undefined" : userObject.user.displayName}</h2>
+        <h2 className="chat-info-header">{userObject.user === undefined ? 'undefined' : userObject.user.displayName}</h2>
         <ChatInfoButtons />
       </div>
-
     </>
-
   );
+};
+
+ChatMenu.propTypes = {
+  userObject: PropTypes.object.isRequired,
 };
 
 export default ChatMenu;
